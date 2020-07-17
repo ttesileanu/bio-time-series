72a112a553a804403e0930de587b116aa6d07a42
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2020-07-17 17:22:39 -0400

    Add Stream source

--- bioslds/sources.py
+++ bioslds/sources.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 
+from typing import Sequence
+
 
 class Constant(object):
     """ A source that always returns the same value.
@@ -23,3 +25,35 @@ class Constant(object):
             Number of values to generate.
         """
         return np.repeat(self.value, size)
+
+
+class Stream(object):
+    """ A source that streams data from an array.
+
+    Attributes
+    ----------
+    data_store
+        Array from which values are returned.
+    ptr_
+        Current location in array.
+    """
+    def __init__(self, data: Sequence):
+        self.data_store = data
+        self.ptr_ = 0
+
+    def __call__(self, size: int) -> np.ndarray:
+        """ Return values from the data store.
+
+        Raises `IndexError` if not enough data is available.
+
+        Parameter
+        ---------
+        n
+            Number of values to return.
+        """
+        if self.ptr_ + size > len(self.data_store):
+            raise IndexError("Ran out of data.")
+
+        data = self.data_store[self.ptr_ : self.ptr_ + size]
+        self.ptr_ += size
+        return data
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -22,5 +22,49 @@ class TestSourcesConstant(unittest.TestCase):
         self.assertEqual(len(y), n)
 
 
+class TestSourcesStreamBasic(unittest.TestCase):
+    def test_empty_result_if_zero_samples_requested_from_empty_store(self):
+        src = sources.Stream([])
+        y = src(size=0)
+
+        self.assertEqual(len(y), 0)
+
+    def test_reading_samples_from_empty_store_raises_index_error(self):
+        src = sources.Stream([])
+        with self.assertRaises(IndexError):
+            y = src(size=1)
+
+
+class TestSourcesStream(unittest.TestCase):
+    def setUp(self):
+        rng = np.random.default_rng(3)
+        self.n = 100
+        self.data = rng.normal(size=self.n)
+        self.src = sources.Stream(self.data)
+
+    def test_empty_result_if_zero_samples_requested(self):
+        y = self.src(size=0)
+
+        self.assertEqual(len(y), 0)
+
+    def test_returned_samples_match_data(self):
+        y = self.src(size=self.n)
+        np.testing.assert_allclose(y, self.data)
+
+    def test_generate_n1_then_n2_returns_first_n1_plus_n2(self):
+        n1 = self.n // 3
+        n2 = self.n // 2
+        y1 = self.src(size=n1)
+        y2 = self.src(size=n2)
+        y = np.hstack((y1, y2))
+
+        data_exp = self.data[: n1 + n2]
+        np.testing.assert_allclose(y, data_exp)
+
+    def test_requesting_too_many_items_raises_index_error(self):
+        with self.assertRaises(IndexError):
+            self.src(size=self.n + 1)
+
+
 if __name__ == '__main__':
     unittest.main()
