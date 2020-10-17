16393ffe5af618e1e9cc2d42e497085afdc43475
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2020-10-17 16:27:16 -0400

    Add scripts to run BioWTA on ARMA(3,3) hyperopt

---
+++ batch/biowta_arma33/run_once.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+export OPENBLAS_NUM_THREADS=1
+
+module load gcc
+module load python3
+
+source env/bin/activate
+
+./run_hyper.py -n500 --n-signals 25 -d$1 -c$1 -o$1 --rate-range 0.00001 0.10 --exp-streak-range 1.0 7.5 --rate-log test_$1.hdf5 3 3 > logs/$1.out 2> logs/$1.err
