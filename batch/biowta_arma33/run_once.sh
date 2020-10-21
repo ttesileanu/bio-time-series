a59b8309ef4f112c213f3e574d54af591569c31c
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2020-10-21 17:34:43 -0400

    Update batch scripts for new run_hyper, new params

--- batch/biowta_arma33/run_once.sh
+++ batch/biowta_arma33/run_once.sh
@@ -6,4 +6,4 @@ module load python3
 
 source env/bin/activate
 
-./run_hyper.py -n500 --n-signals 25 -d$1 -c$1 -o$1 --rate-range 0.00001 0.10 --exp-streak-range 1.0 7.5 --rate-log test_$1.hdf5 3 3 > logs/$1.out 2> logs/$1.err
+./run_hyper.py -n100 --n-signals 25 -d$1 -c$1 -o$1 --rate-range 1.5e-5 0.04 --exp-streak-range 1.5 4.5 --rate-log --economy test_$1.hdf5 3 3 biowta > logs/$1.out 2> logs/$1.err
