8313eef9a0976c3fc8071eda6e15fab06edfe165
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2020-11-23 11:10:05 -0500

    Add rng arg to CepstralRegressor; other:
    * also add n_models, n_features -- needed for rng
    * also add docstring for CepstralRegressor.__init__

--- tests/test_cepstral_regressor.py
+++ tests/test_cepstral_regressor.py
@@ -2,6 +2,8 @@ import unittest
 
 import numpy as np
 
+from unittest import mock
+
 from bioslds.cepstral_regressor import CepstralRegressor
 
 
@@ -32,6 +34,63 @@ class TestCepstralRegressorInit(unittest.TestCase):
 
     def test_cepstral_norm_negative_by_default(self):
         self.assertTrue(self.cepstral.cepstral_norm.negative)
+
+
+class TestCepstralRegressorExceptions(unittest.TestCase):
+    def test_raises_if_n_models_does_not_match_weights_size(self):
+        with self.assertRaises(ValueError):
+            CepstralRegressor(n_models=2, weights=np.zeros((3, 4)), cepstral_order=2)
+
+    def test_raises_if_n_features_does_not_match_weights_size(self):
+        with self.assertRaises(ValueError):
+            CepstralRegressor(n_features=2, weights=np.zeros((3, 4)), cepstral_order=3)
+
+
+class TestCepstralRegressorRng(unittest.TestCase):
+    def test_weights_initialized_using_rng_normal(self):
+        n_models = 2
+        n_features = 3
+
+        rng = mock.Mock()
+        weights = np.reshape(np.arange(n_models * n_features), (n_models, n_features))
+        rng.normal = mock.Mock(return_value=weights)
+
+        cepstral = CepstralRegressor(
+            n_models=n_models, n_features=n_features, rng=rng, cepstral_order=3
+        )
+        np.testing.assert_allclose(cepstral.prediction_error.weights_, weights)
+
+    def test_rng_can_be_seed(self):
+        n_models = 2
+        n_features = 3
+        seed = 43
+
+        cepstral1 = CepstralRegressor(
+            n_models=n_models, n_features=n_features, rng=seed, cepstral_order=3
+        )
+        cepstral2 = CepstralRegressor(
+            n_models=n_models,
+            n_features=n_features,
+            rng=np.random.default_rng(seed),
+            cepstral_order=3,
+        )
+        np.testing.assert_allclose(
+            cepstral1.prediction_error.weights_, cepstral2.prediction_error.weights_
+        )
+
+    def test_default_seed_is_zero(self):
+        n_models = 2
+        n_features = 3
+
+        cepstral1 = CepstralRegressor(
+            n_models=n_models, n_features=n_features, cepstral_order=3
+        )
+        cepstral2 = CepstralRegressor(
+            n_models=n_models, n_features=n_features, rng=0, cepstral_order=3
+        )
+        np.testing.assert_allclose(
+            cepstral1.prediction_error.weights_, cepstral2.prediction_error.weights_
+        )
 
 
 class TestCepstralRegressorStrAndRepr(unittest.TestCase):
