27db0329d120fe13653a7090da54a3bc86bce08d
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2021-01-26 10:55:34 -0500

    Add plot_trajectory utility

--- bioslds/plotting.py
+++ bioslds/plotting.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from matplotlib import patches
+from matplotlib.collections import LineCollection
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from typing import Optional, Sequence, Tuple
 
@@ -296,3 +297,40 @@ def make_gradient_cmap(name: str, c1, c2, **kwargs) -> mplc.Colormap:
 
     cmap = mplc.LinearSegmentedColormap(name, segmentdata=segments, **kwargs)
     return cmap
+
+
+def plot_trajectory(
+        x: Sequence, y: Sequence, hue: Sequence, ax: Optional[plt.Axes] = None, **kwargs
+) -> LineCollection:
+    """ Make a line plot with variable hue.
+
+    Note that this does not autoscale the axes by default. Either set axis limits
+    manually or call `ax.autoscale()`.
+
+    Parameters
+    ----------
+    x
+        Horizontal component.
+    y
+        Vertical component.
+    hue
+        Color component. This is used to map into the colormap.
+    ax
+        Axes in which to draw.
+    Any additional keyword arguments are directly passed to `LineCollection`.
+
+    Returns the `LineCollection` that was drawn.
+    """
+    # adapted from
+    # https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/multicolored_line.html
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+    lc = LineCollection(segments, **kwargs)
+    lc.set_array(np.asarray(hue))
+
+    if ax is None:
+        ax = plt.gca()
+    ax.add_collection(lc)
+
+    return lc
