5431af711761e4eaae363f6e6b49fc913ef80c46
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2021-07-24 08:44:00 -0400

    Add scripts for xcorr vowel hyperopt

---
+++ batch/xcorr_vowels_ao/run_once.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+export OPENBLAS_NUM_THREADS=1
+
+module load gcc
+module load python3
+
+source env/bin/activate
+
+DATA_RNG=1942
+
+./run_hyper_snippets.py \
+    -n50 \
+    --n-signals 200 \
+    -d$DATA_RNG \
+    -c$1 \
+    -o$1 \
+    --normalize \
+    --rate-range 0.0001 0.1 --rate-log \
+    --exp-streak-range 1.0 50.0 --exp-streak-log \
+    --economy \
+    test_$1.hdf5 vowel ao xcorr \
+    > logs/$1.out \
+    2> logs/$1.err
