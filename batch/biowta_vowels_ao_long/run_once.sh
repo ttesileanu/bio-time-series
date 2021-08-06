92693936986fa7ca65fd98b09273820406df6180
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2021-08-06 14:05:22 -0400

    Update hyperopt ranges for long-vowel biowta

--- batch/biowta_vowels_ao_long/run_once.sh
+++ batch/biowta_vowels_ao_long/run_once.sh
@@ -17,10 +17,10 @@ DATA_RNG=1940
     --normalize \
     --average-dwell 1500 \
     --min-dwell 800 \
-    --rate-range 0.0001 0.02 --rate-log \
-    --exp-streak-range 2.0 20.0 --exp-streak-log \
-    --temperature-range 0.1 50.0 --temperature-log \
-    --timescale-range 1.0 100.0 --timescale-log \
+    --rate-range 0.001 0.02 --rate-log \
+    --exp-streak-range 2.0 100.0 --exp-streak-log \
+    --temperature-range 1.0 100.0 --temperature-log \
+    --timescale-range 10.0 150.0 --timescale-log \
     --economy \
     test_$1.hdf5 vowel ao biowta \
     > logs/$1.out \
