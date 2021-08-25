8f7febb130ed8f2a6c6dbfc6679b61588cff54b4
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2021-08-25 16:03:51 -0400

    Extend feature-step-range for xcorr vowel low_p batch run

--- batch/xcorr_vowels_ao_long_low_p/run_once.sh
+++ batch/xcorr_vowels_ao_long_low_p/run_once.sh
@@ -18,9 +18,9 @@ DATA_RNG=1940
     --normalize \
     --average-dwell 1500 \
     --min-dwell 800 \
-    --rate-range 0.000005 0.01 --rate-log \
+    --rate-range 0.000005 0.005 --rate-log \
     --exp-streak-range 2.0 100.0 --exp-streak-log \
-    --feature-step-range 1 250 --feature-step-log \
+    --feature-step-range 1 600 --feature-step-log \
     --economy \
     test_$1.hdf5 vowel ao xcorr \
     > logs/$1.out \
